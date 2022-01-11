4bbd1f295fd463ad3ab932065f8283b9c0ee17ac
dongwhfdyer <1402487376@qq.com>
2022-01-11 20:07:35 +0800

    数据挖掘课程 英文笑话生成20210713

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import torch
 import pandas as pd
 from collections import Counter
+# 数据处理部分最重要
 
+# 首先继承了torch中的datesets的类
 class Dataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -9,14 +11,16 @@ class Dataset(torch.utils.data.Dataset):
     ):
         self.args = args
         self.words = self.load_words()
+        # 特殊词汇是什么
         self.uniq_words = self.get_uniq_words()
-
+        # 建立由词语到索引的和由索引到词语的字典
         self.index_to_word = {index: word for index, word in enumerate(self.uniq_words)}
         self.word_to_index = {word: index for index, word in enumerate(self.uniq_words)}
-
+        # 词语所对应的索引列表
         self.words_indexes = [self.word_to_index[w] for w in self.words]
 
     def load_words(self):
+        # 通过csv文件进行数据收集,这里返回的是单个的单词列表集合
         train_df = pd.read_csv('data/reddit-cleanjokes.csv')
         text = train_df['Joke'].str.cat(sep=' ')
         return text.split(' ')
@@ -33,3 +37,5 @@ class Dataset(torch.utils.data.Dataset):
             torch.tensor(self.words_indexes[index:index+self.args.sequence_length]),
             torch.tensor(self.words_indexes[index+1:index+self.args.sequence_length+1]),
         )
+
+
--- model.py
+++ model.py
@@ -2,32 +2,38 @@ import torch
 from torch import nn
 
 class Model(nn.Module):
+    # 模型含有lstm 和embedding 层
     def __init__(self, dataset):
         super(Model, self).__init__()
         self.lstm_size = 128
         self.embedding_dim = 128
         self.num_layers = 3
-
+        # 存储特殊词汇的数量
         n_vocab = len(dataset.uniq_words)
+        # 嵌入层传参，词汇数量和嵌入层维度
         self.embedding = nn.Embedding(
             num_embeddings=n_vocab,
             embedding_dim=self.embedding_dim,
         )
+        # lstm的输入和隐藏层，dropout 参数为0.2
         self.lstm = nn.LSTM(
             input_size=self.lstm_size,
             hidden_size=self.lstm_size,
             num_layers=self.num_layers,
             dropout=0.2,
         )
+        # 全连接层的输出的个数输入是一致的，都是n_vocab
         self.fc = nn.Linear(self.lstm_size, n_vocab)
 
     def forward(self, x, prev_state):
         embed = self.embedding(x)
+        # 输出和状态是lstm的输出
         output, state = self.lstm(embed, prev_state)
         logits = self.fc(output)
 
         return logits, state
 
+    # 这个函数是为了初始化状态
     def init_state(self, sequence_length):
         return (torch.zeros(self.num_layers, sequence_length, self.lstm_size),
                 torch.zeros(self.num_layers, sequence_length, self.lstm_size))
